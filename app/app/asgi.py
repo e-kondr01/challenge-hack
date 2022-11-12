0437450c9907ebae558ac240367454a6e0cef227
e-kondr01 <e.kondr01@gmail.com>
2022-11-12 17:20:19 +0300

    feat: add jwt auth for ws

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 from donations.ws_routes import websockets_urlpatterns as donations_ws_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
@@ -11,6 +12,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": URLRouter(donations_ws_urlpatterns),
+        "websocket": JWTAuthMiddlewareStack(URLRouter(donations_ws_urlpatterns)),
     }
 )
