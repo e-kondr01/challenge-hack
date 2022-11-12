5f4fcfeb4127b60d2ca2438768dd6fb0c25592ba
e-kondr01 <e.kondr01@gmail.com>
2022-11-12 18:44:09 +0300

    feat: donation consumer handles user-based connections

--- app/donations/consumer.py
+++ app/donations/consumer.py
@@ -1,13 +1,37 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from users.models import User
+
+
+def get_donation_consumer_group_name(user: User) -> str:
+    """
+    Возвращает название группы Channels для донатов пользователю.
+    """
+
+    return f"donations-{user.pk}"
 
 
 class DonationConsumer(AsyncJsonWebsocketConsumer):
-    async def new_donation(self, event):
+    """
+    Consumer для отправки новых донатов, приходящих пользователю.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group_name = ""
+
+    async def donation_new(self, event):
         await self.send_json(event["data"])
 
     async def connect(self):
+        if not self.scope["user"].is_anonymous:
+            # Принимаем только авторизованных пользователей
+            self.group_name = get_donation_consumer_group_name(self.scope["user"])
 
-        self.group_name = "donations"
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+            await self.accept()
 
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, _):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
--- app/donations/signals.py
+++ app/donations/signals.py
@@ -2,16 +2,18 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from donations.consumer import get_donation_consumer_group_name
 
 from .models import Donation
 from .serializers import ReceivedDonationSerializer
 
 
 @receiver(post_save, sender=Donation)
-def notify_new_donation(sender, instance: Donation, created: bool = False, **kwargs):
+def notify_new_donation(_, instance: Donation, created: bool = False, **kwargs):
     if created:
         serializer = ReceivedDonationSerializer(instance)
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            "donations", {"type": "new.donation", "data": serializer.data}
+        group_name = get_donation_consumer_group_name(instance.donated_to)
+        async_to_sync(get_channel_layer().group_send)(
+            group_name,
+            {"type": "donation.new", "data": serializer.data},
         )
