a1dfefe1da01acaff22b5df6c276f4c82813e2a0
e-kondr01 <e.kondr01@gmail.com>
2022-11-12 18:47:00 +0300

    fix: asgi startup

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -1,13 +1,13 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
-from donations.ws_routes import websockets_urlpatterns as donations_ws_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+from donations.ws_routes import websockets_urlpatterns as donations_ws_urlpatterns
 
 application = ProtocolTypeRouter(
     {
